4734b5b5192d3473809460b3dbc640198e74de0a
jp-gerona <julianpetergerona@gmail.com>
2024-01-17 02:51:49 +0800

    Finished home section

--- script/script.js
+++ script/script.js
@@ -1,3 +1,6 @@
+/*
+ * Toggles the open class on the hamburger icon and menu links elements to open/close the menu using the onclick event.
+ */
 function toggleMenu() {
   // Targets the hamburger icon and menu links using the DOM
   const icon = document.querySelector('.menu-links');
