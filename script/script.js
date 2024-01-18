e27cbe6fa73ff1db43ad34b5fbf794ac7e1ec828
jp-gerona <julianpetergerona@gmail.com>
2024-01-19 01:53:26 +0800

    Half finished progress of project section

--- script/script.js
+++ script/script.js
@@ -10,3 +10,32 @@ function toggleMenu() {
   icon.classList.toggle('open');
   menu.classList.toggle('open');
 }
+
+/*
+ * Opens and closes project modals on click
+ */
+// Targets all project modals, img cards, and close buttons
+const projectModals = document.querySelectorAll('.project-modal');
+const imgCards = document.querySelectorAll('.img-card');
+const projectCloseBtns = document.querySelectorAll('.project-close-btn');
+
+// Adds the open class to project modals
+var projectModal = function (modalclick) {
+  projectModals[modalclick].classList.add('open');
+}
+
+// Adds the event listener for adding the open class to project modals when the user clicks the img cards
+imgCards.forEach((imgCard, i) => {
+  imgCard.addEventListener("click", () => {
+    projectModal(i);
+  });
+});
+
+// Removes the open class from project modals when the user clicks the close button
+projectCloseBtns.forEach((projectCloseBtn) => {
+  projectCloseBtn.addEventListener("click", () => {
+    projectModals.forEach((projectModalView) => {
+      projectModalView.classList.remove('open');
+    });
+  });
+});
