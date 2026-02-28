2eff263f2e77b9fde83b0ec71478fbc06028344c
jp-gerona <julianpetergerona@gmail.com>
2026-02-28 22:29:07 +0800

    feat: set up unocss config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,13 @@
 // @ts-check
 import { defineConfig } from "astro/config";
+import UnoCSS from "unocss/astro";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://jp-gerona.github.io/",
+  integrations: [
+    UnoCSS({
+      injectReset: true,
+    }),
+  ],
 });
