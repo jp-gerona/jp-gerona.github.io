16df59f5a6730e42fba53f4f8a7581a59f272bc5
jp-gerona <julianpetergerona@gmail.com>
2026-03-01 10:51:10 +0800

    chore: update unocss config fonts

--- uno.config.ts
+++ uno.config.ts
@@ -1,3 +1,18 @@
-import  { defineConfig } from "unocss";
+import { createLocalFontProcessor } from "@unocss/preset-web-fonts/local";
+import  { defineConfig, presetAttributify, presetWebFonts, presetWind4 } from "unocss";
 
-export default defineConfig({})
+export default defineConfig({
+  presets: [
+    presetAttributify(),
+    presetWind4(),
+    presetWebFonts({
+      provider: "fontshare",
+      themeKey: "font",
+      fonts: {
+        sans: "Satoshi",
+        mono: "Satoshi Mono",
+      },
+      processors: createLocalFontProcessor(),
+    }),
+  ]
+})
