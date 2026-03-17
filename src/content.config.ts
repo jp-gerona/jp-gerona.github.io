be6eea26d080a07ab0f38a786c04390715754996
jp-gerona <julianpetergerona@gmail.com>
2026-03-17 23:57:59 +0800

    chore: update tsconfig alias

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,11 +1,11 @@
+import { glob } from "astro/loaders";
+import { defineCollection } from "astro:content";
 import {
   credentialsSchema,
   indexSchema,
   practicumSchema,
   projectsSchema,
-} from "@src/content/schema";
-import { glob } from "astro/loaders";
-import { defineCollection } from "astro:content";
+} from "@/content/schema";
 
 const index = defineCollection({
   loader: glob({ base: "./src/content/index", pattern: "index.md" }),
