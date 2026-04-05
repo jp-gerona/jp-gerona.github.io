aa942b1ce3c996f96cb37756e0094705f3a112a0
jp-gerona <julianpetergerona@gmail.com>
2026-04-05 23:38:07 +0800

    chore: update eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,4 +1,6 @@
 import antfu from "@antfu/eslint-config";
+import tsParser from "@typescript-eslint/parser";
+import astroParser from "astro-eslint-parser";
 import eslintPluginAstro from "eslint-plugin-astro";
 
 export default antfu(
@@ -13,9 +15,18 @@ export default antfu(
       quotes: "double",
     },
   },
-  ...eslintPluginAstro.configs["jsx-a11y-recommended"],
+  ...eslintPluginAstro.configs["flat/recommended"],
+  ...eslintPluginAstro.configs["flat/jsx-a11y-recommended"],
   {
     files: ["**/*.astro"],
+    languageOptions: {
+      parser: astroParser,
+      parserOptions: {
+        parser: tsParser,
+        extraFileExtensions: [".astro"],
+        sourceType: "module",
+      },
+    },
     rules: {
       "astro/jsx-a11y/alt-text": ["error", { img: ["Image"] }],
     },
